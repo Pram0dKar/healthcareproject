ac6663c94f93e1090d08bdd7b1ece7dc2334deb0
Pram0dKar <pmk5442@psu.edu>
2023-07-30 06:09:27 -0400

    Added test case validations

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const port = 5000
 app.use(express.static('public'));
 var bodyParser = require('body-parser');
 
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/html', express.static(__dirname + 'public/html'))
+app.use('/data', express.static(__dirname + 'public/data'))
+
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
